5e3d8ce539f4bcc2c5961d0d9934a4f5d1caad58
Shayan-Sakhaei <ShayanSakhaeiDev@Gmail.com>
2023-03-28 17:38:38 +0330

    Problem #70.

---
+++ src/main/kotlin/P70.kt
@@ -0,0 +1,18 @@
+fun climbStairs(n: Int): Int {
+    if (n == 1) return 1
+    if (n == 2) return 2
+    val memo = IntArray(n)
+    memo[0] = 1
+    memo[1] = 2
+
+    for (i in 2 until n) {
+        memo[i] = memo[i - 1] + memo[i - 2]
+    }
+
+    return memo[n - 1]
+}
+
+fun main() {
+    val n = 3
+    println(climbStairs(n))
+}
